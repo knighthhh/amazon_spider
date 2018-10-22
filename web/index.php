40085cb4410f516437a20100463bc7d77d80e998
hayden <775340093@qq.com>
2018-10-22 20:16:34 +0800

    update index

--- web/index.php
+++ web/index.php
@@ -7,15 +7,15 @@ define("APP_DEBUG", True);
 define("SITE_URL", "http://127.0.0.1");
 
 //定义前台界面CSS,image,js路径
-define("HOME_CSS_URL", "/mountings/web/Public/Home/css");
-define("HOME_IMAGES_URL", SITE_URL . "/mountings/web/Public/Home/images");
-define("HOME_JS_URL", SITE_URL . "/mountings/web/Public/Home/js");
-define("HOME_PUBLIC", SITE_URL . "/mountings/web/Home/Public");
+define("HOME_CSS_URL", "/amazon_spider/web/Public/Home/css");
+define("HOME_IMAGES_URL", SITE_URL . "/amazon_spider/web/Public/Home/images");
+define("HOME_JS_URL", SITE_URL . "/amazon_spider/web/Public/Home/js");
+define("HOME_PUBLIC", SITE_URL . "/amazon_spider/web/Home/Public");
 
 //定义后台界面CSS,image,js路径
-define("ADMIN_CSS_URL", SITE_URL . "/mountings/web/Public/Admin/css/");
-define("ADMIN_IMAGES_URL", SITE_URL . "/mountings/web/Public/Admin/images/");
-define("ADMIN_JS_URL", SITE_URL . "/mountings/web/Public/Admin/js/");
+define("ADMIN_CSS_URL", SITE_URL . "/amazon_spider/web/Public/Admin/css/");
+define("ADMIN_IMAGES_URL", SITE_URL . "/amazon_spider/web/Public/Admin/images/");
+define("ADMIN_JS_URL", SITE_URL . "/amazon_spider/web/Public/Admin/js/");
 
 define("NCBI_URL", "https://www.ncbi.nlm.nih.gov/pubmed/");
 
