91c6518e6980b143f1deff3643e5f87f3d6e42aa
hayden <775340093@qq.com>
2018-11-09 18:19:04 +0800

    update sort

--- web/Admin/Controller/KeywordController.class.php
+++ web/Admin/Controller/KeywordController.class.php
@@ -15,9 +15,24 @@ class KeywordController extends BaseController
     public function analyze(){
 		$model = D('Keyword_res');
 		$info = $model->search();
-		//dump($info);die;
 		$this->assign($info);
 		$this->display();
+	}
+
+	 public function export(){
+		$model = D('Keyword_res');
+		$info = $model->search();
+		$this->assign($info);
+		$this->display();
+	}
+
+	public function sort_res(){
+		$model = D('Keyword_res');
+		$info = $model->sort_res();
+		$this->assign($info);
+		$this->display(get_res);
+
+
 	}
 
 }
--- web/Admin/Model/KeywordResModel.class.php
+++ web/Admin/Model/KeywordResModel.class.php
@@ -61,6 +61,87 @@ class KeywordResModel extends Model{
         );
 	}
 
+	public function sort_res($perPage=10)
+	{
+	    $price = $_GET['price'];
+	    $comment = $_GET['comment'];
+	    $commentRating = $_GET['commentRating'];
+	    $follow = $_GET['follow'];
+
+        $temparr = array();
+        if($price == '1'){
+            array_push($temparr,'price desc');
+        }else if($price == '2'){
+            array_push($temparr,'price asc');
+        }
+
+        if($comment == '1'){
+            array_push($temparr,'commentCount desc');
+        }else if($comment == '2'){
+            array_push($temparr,'commentCount asc');
+        }
+
+        if($commentRating == '1'){
+            array_push($temparr,'commentRating desc');
+        }else if($commentRating == '2'){
+            array_push($temparr,'commentRating asc');
+        }
+
+        if($follow == '1'){
+            array_push($temparr,'follow_sale_num desc');
+        }else if($follow == '2'){
+            array_push($temparr,'follow_sale_num asc');
+        }
+
+        $orderstr = join(",",$temparr);
+
+        $count = $this->count();
+
+        //实例化翻页类对象
+        $pageObj = new \Think\Page($count, $perPage);
+        $pageObj->setConfig('next', '下一页');
+        $pageObj->setConfig('prev', '上一页');
+        $pageButton = $pageObj->show();
+
+        $data       = $this
+            ->order($orderstr)
+            ->limit($pageObj->firstRow . "," . $pageObj->listRows)
+            ->select();
+
+        //处理跟卖
+        foreach($data as $k=>$v){
+             $product_id = $v['product_id'];
+             $follow_where =  array();
+             $images_where =  array();
+             $follow_where['product_id'] = array('eq',$product_id);
+             $images_where['product_id'] = array('eq',$product_id);
+             $follow_sale = M(Follow_sale)
+                    ->where($follow_where)
+                    ->select();
+             $images = M(Image)
+                    ->where($images_where)
+                    ->select();
+             $data[$k]['follow_sale'] = $follow_sale;
+             $data[$k]['images'] = $images;
+
+        }
+
+         //处理url词频统计
+        foreach($data as $k=>$v){
+            $url = $v['url'];
+            preg_match('/https:\/\/www.amazon.com\/([^.]+)\/dp\//',$url, $matches);
+            $match_res = $matches[1];
+            $split_res = explode("-",$match_res);
+            $res = array_count_values($split_res);
+            $data[$k]['count_url'] = $res;
+        }
+
+        return array(
+            'data' => $data, //用户信息
+            'page' => $pageButton, //分页结果
+        );
+	}
+
 	protected function _before_insert(&$data,$option){
 
 		
