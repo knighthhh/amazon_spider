37dc5dbb2aeaf5e3f61244dd6963fb6c04cfbbde
hayden <775340093@qq.com>
2018-11-11 21:18:22 +0800

    add ajax

--- web/Admin/Controller/BestsellerController.class.php
+++ web/Admin/Controller/BestsellerController.class.php
@@ -4,7 +4,7 @@ use Think\Controller;
 
 class BestsellerController extends BaseController
 {
-	public function get_res(){
+	public function get_bestseller(){
 		$model = D('Keyword_res');
 		$info = $model->search();
 		//dump($info);die;
@@ -13,6 +13,25 @@ class BestsellerController extends BaseController
 	}
 
 
+    public function get_levelone(){
+		$model = M('onedepa');
+        $res = $model -> select();
+    	echo json_encode($res);
+	}
+
+	public function get_leveltwo(){
+		$model = M('twodepa');
+	    $data['pid'] = I('post.pid');
+        $res = $model->where($data) -> select();
+    	echo json_encode($res);
+	}
+
+    public function get_levelthree(){
+		$model = M('threedepa');
+	    $data['pid'] = I('post.pid');
+        $res = $model->where($data) -> select();
+    	echo json_encode($res);
+	}
 }
 
 ?>
